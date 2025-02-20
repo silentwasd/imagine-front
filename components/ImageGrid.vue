c2e30a34d3801dd7ab875724c216cc239776d2e8
BashkirskyKot <silentwasd@gmail.com>
2025-02-21 02:15:26 +0300

    Smoothy gallery

--- components/ImageGrid.vue
+++ components/ImageGrid.vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
-import ImageRepository from "~/repositories/ImageRepository";
-import type TagResource from "~/resources/TagResource";
-import type PaginatedCollection from "~/types/api/PaginatedCollection";
 import type ImageResource from "~/resources/ImageResource";
 
-const tags = ref<TagResource[]>([]);
-
-const imageRepo      = new ImageRepository();
-const {data: images} = await imageRepo.lazyList<PaginatedCollection<ImageResource>>(() => ({
-    tags: tags.value.map(tag => tag.id)
-}));
+defineProps<{
+    images: ImageResource[]
+}>();
 </script>
 
 <template>
     <div class="flex justify-center p-2">
         <div class="grid grid-cols-4 gap-2">
-            <NuxtLink v-for="image in images?.data ?? []"
+            <NuxtLink v-for="image in images"
                       class="w-full"
                       :to="`/image?id=${image.id}`">
                 <img :src="fileUrl(image.preview_path)"
--- pages/image.vue
+++ pages/image.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import ImageRepository from "~/repositories/ImageRepository";
+import type PaginatedCollection from "~/types/api/PaginatedCollection";
+import type ImageResource from "~/resources/ImageResource";
 
 const route   = useRoute();
 const imageId = computed<number>(() => parseInt(route.query.id as string));
@@ -7,24 +9,31 @@ const imageId = computed<number>(() => parseInt(route.query.id as string));
 const imageRepo     = new ImageRepository();
 const {data: image} = await imageRepo.show(`image.${imageId.value}`, imageId.value);
 
+const images = ref<PaginatedCollection<ImageResource>>();
+
 const previewSrc = ref<string>(fileUrl(image.value?.data.preview_path ?? ''));
 const imageSrc   = ref<string>(fileUrl(image.value?.data.path ?? ''));
 const loading    = ref<boolean>(true);
+const imageKey   = ref<number>(1);
 
 watch(imageId, async (id) => {
     loading.value = true;
+    imageKey.value++;
+    console.log(imageKey.value);
 
     previewSrc.value = '';
     imageSrc.value   = '';
 
-    image.value = await imageRepo.get(id);
+    image.value = (images.value?.data ?? []).filter(_image => _image.id == id).map(_image => ({data: _image}))[0] ?? await imageRepo.get(id);
 
     previewSrc.value = fileUrl(image.value?.data.preview_path ?? '');
     imageSrc.value   = fileUrl(image.value?.data.path ?? '');
 });
 
-onMounted(() => {
+onMounted(async () => {
     loading.value = false;
+
+    images.value = await imageRepo.fetchList();
 });
 </script>
 
@@ -33,12 +42,16 @@ onMounted(() => {
         <div v-if="image"
              class="w-1/2 h-dvh shrink-0 bg-cover bg-center bg-no-repeat"
              :style="`background-image: url(${previewSrc});`">
-            <div class="relative flex items-center justify-center w-full h-full backdrop-blur">
-                <img :src="imageSrc"
+            <div class="relative flex items-center justify-center w-full h-full overflow-clip">
+                <img :key="`image-${imageKey}`"
+                     :src="imageSrc"
                      @load="loading = false"
-                     class="max-h-dvh"/>
+                     class="absolute max-h-dvh z-10 transition-all"
+                     :class="{'opacity-0 scale-125': loading, 'opacity-100 scale-100': !loading}"/>
 
-                <div class="absolute w-full h-full flex items-center justify-center transition-opacity"
+                <div class="absolute w-full h-full backdrop-blur"></div>
+
+                <div class="absolute w-full h-full flex items-center justify-center transition-opacity z-10"
                      :class="{'opacity-0': !loading, 'opacity-100': loading}">
                     <UIcon class="animate-spin text-8xl text-gray-50/90"
                            name="i-heroicons-arrow-path-20-solid"/>
@@ -47,7 +60,8 @@ onMounted(() => {
         </div>
 
         <ClientOnly>
-            <ImageGrid class="grow w-0 overflow-auto h-dvh"/>
+            <ImageGrid class="grow w-0 overflow-auto h-dvh"
+                       :images="images?.data ?? []"/>
         </ClientOnly>
     </div>
 </template>
